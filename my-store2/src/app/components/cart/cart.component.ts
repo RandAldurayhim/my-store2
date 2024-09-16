d0779c0f113dfab7980ce273fb9c98c75c62acf6
rand-02 <randabdulaziz14@gmail.com>
2024-09-16 14:29:40 +0300

    added button remove & format & added removeCartItem function

--- my-store2/src/app/components/cart/cart.component.ts
+++ my-store2/src/app/components/cart/cart.component.ts
@@ -6,77 +6,90 @@ import { CartService } from '../../services/cart/cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
-}
-)
+  styleUrl: './cart.component.css',
+})
 export class CartComponent implements OnInit {
   fname = '';
   address = '';
   creditCard = '';
   totalPrice = 0;
-  constructor(private router:Router ,  private cartService : CartService) {}
-  cartList: CartItem[]=[];
+  constructor(private router: Router, private cartService: CartService) {}
+  cartList: CartItem[] = [];
 
-ngOnInit(): void {
-  this.fname = this.cartService.getName();
-  this.address = this.cartService.getAddress();
-  this.creditCard = this.cartService.getCreditCard();
-  this.cartList = this.cartService.getCartItems();
-  this.totalPrice = this.cartList.reduce((acc, item) => acc + (item.price * item.amount), 0);
-}
-onSubmit():void{
+  ngOnInit(): void {
+    this.fname = this.cartService.getName();
+    this.address = this.cartService.getAddress();
+    this.creditCard = this.cartService.getCreditCard();
+    this.cartList = this.cartService.getCartItems();
+    this.totalPrice = this.cartList.reduce(
+      (acc, item) => acc + item.price * item.amount,
+      0
+    );
+  }
 
-   this.cartService.setName(this.fname);
-   this.cartService.setTotalPrice(this.totalPrice);
-   this.router.navigate(['/confirmation']);
-   this.cartService.clearCart();
+  onSubmit(): void {
+    this.cartService.setName(this.fname);
+    this.cartService.setTotalPrice(this.totalPrice);
+    this.router.navigate(['/confirmation']);
+    this.cartService.clearCart();
+  }
 
-}
-
-
-updateCartItem(cartItem: CartItem) {
-  if (cartItem.amount === 0) {
+  removeCartItem(cartItem: CartItem) {
     this.cartService.removeFromCart(cartItem.id);
-    this.cartList = this.cartList.filter(item => item.id !== cartItem.id);
-    alert("Item removed from cart!");
+    this.cartList = this.cartList.filter((item) => item.id !== cartItem.id);
+    this.totalPrice = this.cartList.reduce(
+      (acc, item) => acc + item.price * item.amount,
+      0
+    );
+
+    alert('Item removed from cart!');
   }
-  else if (isNaN(cartItem.amount) || cartItem.amount < 1) {
-    cartItem.amount = 1;
-  } else {
-    cartItem.amount = Math.floor(cartItem.amount);
+
+  updateCartItem(cartItem: CartItem) {
+    if (cartItem.amount === 0) {
+      this.cartService.removeFromCart(cartItem.id);
+      this.cartList = this.cartList.filter((item) => item.id !== cartItem.id);
+      alert('Item removed from cart!');
+    } else if (isNaN(cartItem.amount) || cartItem.amount < 1) {
+      cartItem.amount = 1;
+    } else {
+      cartItem.amount = Math.floor(cartItem.amount);
+    }
+
+    this.cartService.updateCartItem(cartItem);
+    this.totalPrice = this.cartList.reduce(
+      (acc, item) => acc + item.price * item.amount,
+      0
+    );
   }
 
-  this.cartService.updateCartItem(cartItem);
-  this.totalPrice = this.cartList.reduce((acc, item) => acc + (item.price * item.amount), 0);
-}
-updateName(fname: string) {
-  if (this.validateName(fname)) {
-    this.cartService.setName(fname);
+  updateName(fname: string) {
+    if (this.validateName(fname)) {
+      this.cartService.setName(fname);
+    }
   }
-}
 
-updateAddress(address: string) {
-  if (this.validateAddress(address)) {
-    this.cartService.setAddress(address);
+  updateAddress(address: string) {
+    if (this.validateAddress(address)) {
+      this.cartService.setAddress(address);
+    }
   }
-}
 
-updateCreditCard(creditCard: string) {
-  if (this.validateCreditCard(creditCard)) {
-    this.cartService.setCreditCard(creditCard);
+  updateCreditCard(creditCard: string) {
+    if (this.validateCreditCard(creditCard)) {
+      this.cartService.setCreditCard(creditCard);
+    }
   }
-}
 
-private validateName(name: string): boolean {
-  return name.length >= 3;
-}
+  private validateName(name: string): boolean {
+    return name.length >= 3;
+  }
 
-private validateAddress(address: string): boolean {
-  return address.length >= 6;
-}
+  private validateAddress(address: string): boolean {
+    return address.length >= 6;
+  }
 
-private validateCreditCard(creditCard: string): boolean {
-  return creditCard.length === 16 && !isNaN(Number(creditCard));
-}
-
+  private validateCreditCard(creditCard: string): boolean {
+    return creditCard.length === 16 && !isNaN(Number(creditCard));
+  }
 }
